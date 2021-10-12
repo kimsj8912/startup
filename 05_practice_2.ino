e77dbfbf6a2577b23e14e52b699bdf8ea2dc0b31
kimseojeong <kimsj8912@kookmin.ac.kr>
2021-10-12 17:05:56 +0900

    수정 사항 주석 추가
    수정 사항에 대한 주석 추가하였습니다.

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -9,13 +9,13 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(7, 0);
+  digitalWrite(7, 0); // 1초동안 LED를 켬
   delay(1000);
-  for (i = 1; i <= 10; i++) {
-    digitalWrite(7, i % 2);
-    delay(100);
+  for (i = 1; i <= 10; i++) { // LED를 5번 깜빡이기 위해 10회 반복
+    digitalWrite(7, i % 2); // i % 2의 결과값은 0과 1이 반복적으로 등장
+    delay(100); // 꺼진 상태 또는 켜진 상태를 100ms 동안 유지
   }
   while(1) {
-    digitalWrite(7, 1);
+    digitalWrite(7, 1); // LED가 꺼진 상태
   }
 }
